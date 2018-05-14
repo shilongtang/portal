ed6972556a08c95b948737007d0617ba924df4c8
shilong.tang <shilong.tang@grandinsight.com>
2018-05-14 11:33:33 +0800

    demo按钮修改

--- src/apps/demo/desktop.vue
+++ src/apps/demo/desktop.vue
@@ -2,11 +2,11 @@
   <panel>
   <el-row>
     <el-button>默认按钮</el-button>
-    <el-button type="primary" @click="loding">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <el-button type="primary" @click="loding('1')">主要按钮</el-button>
+    <el-button type="success" @click="loding('2')">成功按钮</el-button>
+    <el-button type="info" @click="loding('3')">信息按钮</el-button>
+    <el-button type="warning" @click="loding('4')">警告按钮</el-button>
+    <el-button type="danger" @click="loding('5')">危险按钮</el-button>
   </el-row>
     <el-input v-model="input"></el-input>
     <el-input v-model="input"></el-input>
@@ -29,10 +29,18 @@
 
     },
     methods: {
-        loding(){
-          muenList().then(result=>{
-            console.error("xxx",...result.body.list);
-          })
+        loding(type){
+         if(type === '1'){
+           this.$message('主要按钮');
+         }else if(type === '2'){
+           this.$message.success('成功按钮');
+         }else if(type === '3'){
+           this.$message('信息按钮');
+         }else if(type === '4'){
+           this.$message.warning('警告按钮');
+         }else if(type === '5'){
+           this.$message.error('危险按钮');
+         }
         }
     }
   };
